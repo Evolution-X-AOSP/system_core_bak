e4304ef3b7b4136e5a92618eea3e6bbf26e1c439
beanstown106 <nbenis106@gmail.com>
2012-02-05 00:55:17 +0100

    Set /system/etc/init.d/* permissions
    Change-Id: I86fd9333beb328f542444df6941074546b8b49be
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: c8580748ca7966720fcf28a945eeed340926c11a
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -83,6 +83,7 @@ static const struct fs_path_config android_dirs[] = {
     { 00777, AID_ROOT,         AID_ROOT,         0, "sdcard" },
     { 00751, AID_ROOT,         AID_SDCARD_R,     0, "storage" },
     { 00751, AID_ROOT,         AID_SHELL,        0, "system/bin" },
+    { 00755, AID_ROOT,         AID_SHELL,        0, "system/etc" },
     { 00755, AID_ROOT,         AID_ROOT,         0, "system/etc/ppp" },
     { 00755, AID_ROOT,         AID_SHELL,        0, "system/vendor" },
     { 00751, AID_ROOT,         AID_SHELL,        0, "system/xbin" },
