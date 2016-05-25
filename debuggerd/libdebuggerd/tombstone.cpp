18865b780c0f49b88dcffdd5c7903c101031a8ec
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add Evolution X version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to Evolution X
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    Former-commit-id: 8ed66d57ba28b1884720d9895c6b62d2c9b3a436
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -72,7 +72,9 @@ using namespace std::literals::string_literals;
 static void dump_header_info(log_t* log) {
   auto fingerprint = GetProperty("ro.build.fingerprint", "unknown");
   auto revision = GetProperty("ro.revision", "unknown");
+  auto evolution_version = GetProperty("org.evolution.version", "unknown");
 
+  _LOG(log, logtype::HEADER, "Evolution X Version: '%s'\n", evolution_version.c_str());
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint.c_str());
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision.c_str());
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
